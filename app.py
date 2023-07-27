f5eac510ec970f3528828c22eafb615794e0a589
Hannah <hannahm@star.sr.bham.ac.uk>
2023-07-27 13:15:20 +0100

    getting ready for data release v2

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import h5py
 import matplotlib.pyplot as plt
 import json
 import pickle
-import argparse
+#import argparse
 import pesummary
-from pesummary.io import read
+#from pesummary.io import read
 #from peutils import *
 #from makewaveform import make_waveform, plot_gwtc1_waveform
 #from makealtair import make_altair_plots, get_params_intersect
@@ -47,44 +47,47 @@ def downloadData(directory):
     return None
 
 
-events = ['GW191103A', \
-          'GW191105C', \
-          'GW191109A', \
-          'GW191113B', \
-          'GW191126C', \
-          'GW191127B', \
-          'GW191129G', \
-          'GW191204A', \
-          'GW191204G', \
-          'GW191215G', \
-          'GW191216G', \
-          'GW191219E', \
-          'GW191222A', \
-          'GW191230H', \
-          '200105F', \
-          'GW200112H', \
-          'GW200115A', \
-          'GW200128C', \
-          'GW200129D', \
-          'GW200202F', \
-          'GW200208G', \
-          'GW200208K', \
-          'GW200209E', \
-          'GW200210B', \
-          'GW200216G', \
-          'GW200219D', \
-          'GW200220E', \
-          'GW200224H', \
-          'GW200225B', \
-          'GW200302A', \
-          'GW200306A', \
-          'GW200308G', \
-          'GW200311L', \
-          'GW200316I', \
+events = ['GW191103A',
+          'GW191105C',
+          'GW191109A',
+          'GW191113B',
+          'GW191126C',
+          'GW191127B',
+          'GW191129G',
+          'GW191204A',
+          'GW191204G',
+          'GW191215G',
+          'GW191216G',
+          'GW191219E',
+          'GW191222A',
+          'GW191230H',
+          '200105F',
+          'GW200112H',
+          'GW200115A',
+          'GW200128C',
+          'GW200129D',
+          'GW200202F',
+          'GW200208G',
+          'GW200208K',
+          'GW200209E',
+          'GW200210B',
+          'GW200216G',
+          'GW200219D',
+          'GW200220E',
+          'GW200220H',
+          'GW200224H',
+          'GW200225B',
+          'GW200302A',
+          'GW200306A',
+          'GW200308G',
+          'GW200311L',
+          'GW200316I',
           'GW200322G']
+
 
 #posHighlight = ['GW191230A', 'GW191109A', 'GW191129A', \
 #                 'GW200115A', '200105A', 'GW200311A']
+
 
 highlightDefaults = ['GW191204_171526', \
                      'GW191219_163120', \
@@ -92,15 +95,7 @@ highlightDefaults = ['GW191204_171526', \
                      'GW200115_042309', \
                      'GW200210_092254', \
                      'GW200220_061928', \
-                     'GW200225_060421']
-
-#highlightDefaults = ['GW191109_010717', \
-#                     'GW191215_223052', \
-#                     'GW191230_180458', \
-#                     'GW200105_162426', \
-#                     'GW200115_042309', \
-#                     'GW200210_092254', \
-#                     'GW200316_215756' ]
+                     'GW200225_06042']
 
 
 # get namaees
@@ -113,6 +108,8 @@ print('\n\n names', type(names['FULLNAME']))
 eventsFullNames = [ names['FULLNAME'][e] for e in events]
 
 
+
+st.set_page_config(page_title='GW Contour Plotter', page_icon=":crocodile:")
 
 # sidebar stuff
 highlightsSelected = st.sidebar.multiselect('Event to highlight',
@@ -128,18 +125,30 @@ eventsSelected = st.sidebar.multiselect('Events to be plotted (the default is al
 
 
 
-st.title('GWTC-3 contour plotter')
+
+st.title('GWTC-3 Contour Plots')
 
 
 st.markdown('''
-**This is a work in progress!**
+GWTC-3 is the "third gravitational wave transient catalog" from the [LIGO](https://www.ligo.org/), [Virgo](https://www.virgo-gw.eu/), and [KAGRA](https://gwcenter.icrr.u-tokyo.ac.jp/en/) (LVK) Collaborations.
+[GWTC-3](https://www.ligo.org/science/Publication-O3bCatalog/) updates the previous catalogs ([GWTC-1](https://www.ligo.org/science/Publication-O2Catalog/), [GWTC-2](https://www.ligo.org/science/Publication-O3aCatalog/), [GWTC-2.1](https://www.ligo.org/science/Publication-O3aFinalCatalog/)) with merger events observed during the second part of Observing Run 3 (O3b), which lasted from the 1st November 2019 to the 27th March 2020. 
 
-This app shows selected parameter estimation results for gravitational wave events observed in the second part of the [LIGO](https://www.ligo.org/), [Virgo](https://www.virgo-gw.eu/), [KAGRA](https://gwcenter.icrr.u-tokyo.ac.jp/en/) Observing Run 3 (O3b). 
+As in the [GWTC-3 paper](https://arxiv.org/abs/2111.03606), the gravitational-wave events included in these plots are those that have a greater than 50% probability of being real astrophysical signals. 
+Identical to the paper, we include one additional event (GW200105_162426) which, despite having a lower probability of being real, is still a clear outlier from the noise background (see the paper for more information).
+The plots reproduce figures 8 and 9 from the paper. 
+''')
 
-**How to use this app**
 
-Change which events to plot and highlight in the sidebar. Highlighting fewer events gives the best results as the plot can get quite cluttered! Choose which parameter combination you would like to plot below. 
 
+
+st.subheader('How to use this app')
+
+st.markdown('''
+You can customise which gravitational-wave events to plot (gray contours) and/or highlight (color contours).
+Choose which events to plot and highlight in the sidebar. 
+Highlighting fewer events gives the best results as the plot can get quite cluttered!
+
+Select which parameter combination you would like to see below. 
 ''')
 
 
